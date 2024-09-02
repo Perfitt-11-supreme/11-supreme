10b188070e78e35662898885fc17dde1f7faa091
deswaq1220 <deswaq789@naver.com>
2024-09-02 16:49:15 +0900

    :tada:

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>폰트 적용 테스트</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css.ts'
+
+const queryClient = new QueryClient()
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+)
