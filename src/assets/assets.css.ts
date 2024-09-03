4ef48379e1a692b2f8b1b13524b059a01c94265c
deswaq1220 <deswaq789@naver.com>
2024-09-03 19:33:13 +0900

    add: 추천 운동화 컴포넌트 아이콘 추가

--- src/assets/assets.css.ts
+++ src/assets/assets.css.ts
@@ -3,12 +3,14 @@ import back_arrow from './back_arrow.svg';
 import bar from './bar.svg';
 import brand_abcmart from './brand_abcmart.svg';
 import checked from './checked.svg';
+import detail from './detail.svg';
 import export_icon from './export.svg';
 import hamburger_menu from './hamburger-menu.svg';
 import heart_empty from './heart_empty.svg';
 import heart_filled from './heart_filled.svg';
 import prefitt_symbol from './perfitt_symbol.svg';
 import picture from './picture.svg';
+import polygon2 from './polygon2.svg';
 import select_arrow20 from './select_arrow20.svg';
 import select_arrow24 from './select_arrow24.svg';
 import shoes_w159 from './shoes_w159.svg';
@@ -19,7 +21,7 @@ import thumbsDown from './thumbsDown.svg';
 import upload from './upload.svg';
 export {
   ai,
-  back_arrow, bar, brand_abcmart, checked, export_icon, hamburger_menu, heart_empty, heart_filled, picture, prefitt_symbol, select_arrow20,
+  back_arrow, bar, brand_abcmart, checked, detail, export_icon, hamburger_menu, heart_empty, heart_filled, picture, polygon2, prefitt_symbol, select_arrow20,
   select_arrow24,
   shoes_w159,
   shoes_w182, small_arrow, speech_bubble, thumbsDown, upload
