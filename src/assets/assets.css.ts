a540b117724c636bc2d549e3c8b622b06c9b022a
deswaq1220 <deswaq789@naver.com>
2024-09-03 15:53:04 +0900

    add: 에셋파일 추가

--- src/assets/assets.css.ts
+++ src/assets/assets.css.ts
@@ -3,28 +3,25 @@ import back_arrow from './back_arrow.svg';
 import bar from './bar.svg';
 import brand_abcmart from './brand_abcmart.svg';
 import checked from './checked.svg';
+import export_icon from './export.svg';
 import hamburger_menu from './hamburger-menu.svg';
-import heart_filled from './heart_filled.svg';
 import heart_empty from './heart_empty.svg';
+import heart_filled from './heart_filled.svg';
+import prefitt_symbol from './perfitt_symbol.svg';
 import picture from './picture.svg';
 import select_arrow20 from './select_arrow20.svg';
 import select_arrow24 from './select_arrow24.svg';
 import shoes_w159 from './shoes_w159.svg';
 import shoes_w182 from './shoes_w182.svg';
+import small_arrow from './small_arrow.svg';
+import speech_bubble from './speech_bubble.svg';
+import thumbsDown from './thumbsDown.svg';
 import upload from './upload.svg';
 export {
   ai,
-  back_arrow,
-  brand_abcmart,
-  bar,
-  checked,
-  hamburger_menu,
-  heart_filled,
-  heart_empty,
-  picture,
-  select_arrow20,
+  back_arrow, bar, brand_abcmart, checked, export_icon, hamburger_menu, heart_empty, heart_filled, picture, prefitt_symbol, select_arrow20,
   select_arrow24,
   shoes_w159,
-  shoes_w182,
-  upload,
+  shoes_w182, small_arrow, speech_bubble, thumbsDown, upload
 };
+
