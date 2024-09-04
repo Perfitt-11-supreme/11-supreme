fe02458557b758c5e792a71ef2af407f59c830d9
deswaq1220 <deswaq789@naver.com>
2024-09-04 16:05:30 +0900

    refactor: 모달 상태 전역상태관리로 뺐음

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
-import { useState } from 'react';
 import { bar } from '../../../assets/assets.css';
+import useModalStore from '../../../stores/useModalStore';
 import { barBox, modalContainer, modalContainerTitle, modalContainerWrapper } from './modal.css';
 
 type TModal = {
@@ -10,26 +10,43 @@ type TModal = {
 };
 
 const Modal = ({ title, height, children }: TModal) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { isOpen, setIsOpen } = useModalStore();
 
   const handleModalClick = () => {
-    setIsOpen(!isOpen)
+    if (title !== '관심 키워드') {
+      setIsOpen(!isOpen);
+    }
   }
+
+  const isInterestKeyword = title === '관심 키워드';
+
   return (
     <>
-      <div className={modalContainerWrapper}> {/* 모달 컨테이너 래퍼 추가 */}
-        <motion.div
-          className={modalContainer}
-          initial={{ height: '125px' }}
-          animate={{ height: isOpen ? height : '125px' }}
-          transition={{ duration: 0.5 }}
-        >
-          <div className={barBox}>
-            {title === '관심키워드' ? null : <img src={bar} alt="bar" onClick={handleModalClick} />}
+      <div className={modalContainerWrapper}>
+        {isInterestKeyword ? (
+          <div
+            className={modalContainer}
+            style={{ height: height }}
+          >
+            <div className={barBox} />
+
+            {title && <h1 className={modalContainerTitle}>{title}</h1>}
+            {children}
           </div>
-          {title && <h1 className={modalContainerTitle}>{title}</h1>}
-          {children}
-        </motion.div>
+        ) : (
+          <motion.div
+            className={modalContainer}
+            initial={{ height: '125px' }}
+            animate={{ height: isOpen ? height : '125px' }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className={barBox} onClick={handleModalClick}>
+              <img src={bar} alt="bar" />
+            </div>
+            {title && <h1 className={modalContainerTitle}>{title}</h1>}
+            {children}
+          </motion.div>
+        )}
       </div>
     </>
   );
