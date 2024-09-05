ed5fe84318a352e6fed8679c43f2135cdcf049e5
IMIMIMIMIMIMIMIM <ism1124@naver.com>
2024-09-05 17:50:09 +0900

    style-onboarding

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,16 @@
-<<<<<<< HEAD
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css.ts'
-import { router } from './router/index.tsx'
-=======
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css.ts';
 import { RouterProvider } from 'react-router-dom';
-import router from './router/index.tsx';
->>>>>>> 002be67 (style: onboarding)
+import { router } from './router/index.tsx';
+import './index.css.ts';
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
       <RouterProvider router={router} />
-<<<<<<< HEAD
-
-=======
->>>>>>> 002be67 (style: onboarding)
     </StrictMode>
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,40 +1,14 @@
-<<<<<<< HEAD
-import { createBrowserRouter } from "react-router-dom";
-import BridgePage from "../pages/bridge-page/bridgePage";
-import ChatBotPage from "../pages/chatbot-page/chatBotPage";
+import { createBrowserRouter } from 'react-router-dom';
+import BridgePage from '../pages/bridge-page/bridgePage';
+import ChatBotPage from '../pages/chatbot-page/chatBotPage';
 
 export const router = createBrowserRouter([
   {
-    path: "/chatBot",
-    element: <ChatBotPage />
+    path: '/chatBot',
+    element: <ChatBotPage />,
   },
   {
-    path: "/redirect",
-    element: <BridgePage />
-  }
-])
-=======
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import OnBoarding from '../components/onboarding/OnBoarding';
-import OnBoarding2 from '../components/onboarding/OnBoarding2';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: 'onBoarding',
-        element: <OnBoarding />,
-      },
-      {
-        path: 'onBoarding2',
-        element: <OnBoarding2 />,
-      },
-    ],
+    path: '/redirect',
+    element: <BridgePage />,
   },
 ]);
-
-export default router;
->>>>>>> 002be67 (style: onboarding)
