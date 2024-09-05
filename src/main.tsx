4bc9da1d224539a2339e27e9303b3ec1f88063d9
IMIMIMIMIMIMIMIM <ism1124@naver.com>
2024-09-05 11:24:16 +0900

    style: onboarding

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {
   QueryClient,
   QueryClientProvider,
@@ -8,14 +9,26 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css.ts'
 import { router } from './router/index.tsx'
+=======
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css.ts';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/index.tsx';
+>>>>>>> 002be67 (style: onboarding)
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
       <RouterProvider router={router} />
+<<<<<<< HEAD
 
+=======
+>>>>>>> 002be67 (style: onboarding)
     </StrictMode>
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
-)
+);
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { createBrowserRouter } from "react-router-dom";
 import BridgePage from "../pages/bridge-page/bridgePage";
 import ChatBotPage from "../pages/chatbot-page/chatBotPage";
@@ -12,3 +13,28 @@ export const router = createBrowserRouter([
     element: <BridgePage />
   }
 ])
+=======
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import OnBoarding from '../components/onboarding/OnBoarding';
+import OnBoarding2 from '../components/onboarding/OnBoarding2';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'onBoarding',
+        element: <OnBoarding />,
+      },
+      {
+        path: 'onBoarding2',
+        element: <OnBoarding2 />,
+      },
+    ],
+  },
+]);
+
+export default router;
+>>>>>>> 002be67 (style: onboarding)
