23c378e16df64c60b352a0b5bcdc52e7ae767aa1
laurenCho9 <lauren.choux@gmail.com>
2024-09-06 16:51:35 +0900

    style: 사이드바메뉴페이지 ui 완성, 좋아요/최근 본 페이지 ui 작업중 중간 커밋

--- src/pages/liked-and-viewedhistory.tsx/likedAndViewedHistory.css.ts
+++ src/pages/liked-and-viewedhistory.tsx/likedAndViewedHistory.css.ts
@@ -4,9 +4,12 @@ import { theme } from '../../styles/theme';
 export const LikedAndViewedHistoryCointainer = style({
   width: '375px',
   height: '812px',
+  position: 'relative',
 });
 export const LikedAndViewedHistoryButtonBox = style({
   width: '100%',
+  position: 'absolute',
+  top: '36.5px',
 });
 export const LikedAndViewedHistoryButton = style({
   width: '50%',
@@ -20,11 +23,30 @@ export const LikedAndViewedHistoryButton = style({
   letterSpacing: '-0.003em',
   textAlign: 'center',
   borderBottom: `1px solid  ${theme.color.slate200}`,
+  cursor: 'pointer',
 });
 export const FilterProductsAndBrandsButtonBox = style({
-  width: '100%',
+  marginTop: '39px',
+  display: 'flex',
+  gap: '5px',
+  padding: '0 16px',
 });
-export const FilterProductsAndBrandsButton = style({});
+export const FilterProductsAndBrandsButton = style({
+  height: '32px',
+  borderRadius: '99px',
+  padding: '4px 12px',
+  fontWeight: '800',
+  fontSize: '16px',
+  lineHeight: '24px',
+  letterSpacing: '-0.003em',
+  textAlign: 'center',
+  color: theme.color.white,
+  backgroundColor: theme.color.black,
+  border: `1px solid ${theme.color.black}`,
+  cursor: 'pointer',
+});
+export const FilterProductsAndBrandsQuantityBox = style({ padding: '0 16px' });
+export const FilterProductsAndBrandsQuantity = style({});
 export const LikedAndViewedHistoryItemBox = style({
   padding: '0 16px',
   display: 'grid',
