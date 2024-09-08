3862f59ba9f693e5c18d67d61f00da0a1927510a
deswaq1220 <deswaq789@naver.com>
2024-09-08 22:13:56 +0900

    chore: 충돌 정리

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,32 +1,42 @@
 
+import again from './again.svg';
 import ai from './ai.svg';
 import analyze from './analyze.svg';
 import analyze2 from './analyze2.svg';
-
+import arrow_down from './arrow_down.svg';
 import arrow_right from './arrow_right.svg';
-
+import arrow_up from './arrow_up.svg';
 import asics from './asics.svg';
 import back_arrow from './back_arrow.svg';
 import bar from './bar.svg';
 import brand_abcmart from './brand_abcmart.svg';
+import brand_adidas from './brand_adidas.svg';
+import brand_crocs from './brand_crocs.svg';
+import brand_nike from './brand_nike.svg';
 import camera from './camera.svg';
 import checked from './checked.svg';
-
+import circle from './circle.svg';
 import close from './close.svg';
+import deleted from './delete.svg';
 import detail from './detail.svg';
 import empty_star from './empty_star.svg';
 import export_icon from './export_icon.svg';
 import filled_star from './filled_star.svg';
-
+import gallery from './gallery.svg';
 import hamburger_menu from './hamburger-menu.svg';
 import heart_empty from './heart_empty.svg';
 import heart_filled from './heart_filled.svg';
 import hoka from './hoka.svg';
 import info from './info.svg';
 import kakao from './kakao.svg';
+import kebab from './kebab.svg';
+import modify from './modify.svg';
 import more_arrow from './more_arrow.svg';
 import my_foot from './my_foot.svg';
 import my_foot_info from './my_foot_info.svg';
+import mypage_arrow from './mypage_arrow.svg';
+import mypage_heart from './mypage_heart.svg';
+import mypage_shose_room from './mypage_shose_room.svg';
 import naver from './naver.svg';
 import noshoes from './noshoes.svg';
 import onboarding1 from './onboarding1.svg';
@@ -36,21 +46,6 @@ import perfittlogo from './perfittlogo.svg';
 import picture from './picture.svg';
 import plus from './plus.svg';
 import polygon2 from './polygon_2.svg';
-
-import again from './again.svg';
-import arrow_down from './arrow_down.svg';
-import arrow_up from './arrow_up.svg';
-import brand_adidas from './brand_adidas.svg';
-import brand_crocs from './brand_crocs.svg';
-import brand_nike from './brand_nike.svg';
-import circle from './circle.svg';
-import deleted from './delete.svg';
-import gallery from './gallery.svg';
-import kebab from './kebab.svg';
-import modify from './modify.svg';
-import mypage_arrow from './mypage_arrow.svg';
-import mypage_heart from './mypage_heart.svg';
-import mypage_shose_room from './mypage_shose_room.svg';
 import rectangle from './rectangle.svg';
 import salomon from './salomon.svg';
 import search from './search_icon.svg';
