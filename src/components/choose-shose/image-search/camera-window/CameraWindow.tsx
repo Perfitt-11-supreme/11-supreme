48fcec957272dd556f105cd45aa797e27a032ee7
JoBeomJun <oreasv3@gmail.com>
2024-09-09 10:39:01 +0900

    refactor-chooseshose---jobeomjun

--- src/components/choose-shose/image-search/camera-window/CameraWindow.tsx
+++ src/components/choose-shose/image-search/camera-window/CameraWindow.tsx
@@ -1,26 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { close, rectangle } from '../../../../assets/assets';
-import CameraComponent from './cameracomponent/CameraComponent';
 import { CameraWindow_CloseIcon, CameraWindow_RectangleContainer } from './camerawindow.css';
 import useAnalyzeStore from '../../../../stores/useAnalyzeStore';
 
-const CameraWindow = ({ videoRef }: { videoRef: React.MutableRefObject<HTMLVideoElement | null> }) => {
-  const { isAnalyze } = useAnalyzeStore();
+const CameraWindow = () => {
+  const { isAnalyze, setIsAnalyze } = useAnalyzeStore();
   const navigate = useNavigate();
 
   const handleNavigation = () => {
+    setIsAnalyze(false);
     navigate('/textSearch');
   };
 
   return (
     <>
-      <CameraComponent videoRef={videoRef} />
+      <img className={CameraWindow_CloseIcon} src={close} alt="close" onClick={handleNavigation} />
       {!isAnalyze && (
         <div className={CameraWindow_RectangleContainer}>
           <img src={rectangle} alt="rectangle" />
         </div>
       )}
-      <img className={CameraWindow_CloseIcon} src={close} alt="close" onClick={handleNavigation} />
     </>
   );
 };
--- src/stores/useCaptureStore.ts
+++ src/stores/useCaptureStore.ts
@@ -2,12 +2,18 @@ import { create } from 'zustand';
 
 type CaptureStore = {
   capturedImage: string | null;
-  setCapturedImage: (capturedImage: string | null) => void;
+  setCapturedImage: (capturedImage: string) => void;
+  handleCaptureImage: () => void;
+  handleClickCameraIcon: (bol: boolean) => void;
+  handleClickAgain: () => void;
 };
 
 const useCaptureStore = create<CaptureStore>(set => ({
   capturedImage: null,
   setCapturedImage: capturedImage => set({ capturedImage }),
+  handleCaptureImage: () => {},
+  handleClickCameraIcon: () => {},
+  handleClickAgain: () => {},
 }));
 
 export default useCaptureStore;
