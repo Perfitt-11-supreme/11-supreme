d2b8983d68b3bb821519a53bdade668cf7f92c51
deswaq1220 <deswaq789@naver.com>
2024-09-25 21:47:09 +0900

    chore: 공통 스타일 수정

--- src/styles/responsive.css.ts
+++ src/styles/responsive.css.ts
@@ -2,21 +2,20 @@ import { style } from '@vanilla-extract/css';
 import { theme } from './theme';
 
 export const responsiveBox = style({
-  maxWidth:'375px',
+  maxWidth:'428px',
   width: '100%',
   height: '100%',
-  maxHeight:'812px',
+  maxHeight:'100%',
   backgroundColor: theme.color.white,
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  borderRadius: '20px',
   display: 'flex',
   flexDirection: 'column', // 세로로 배치하여 레이아웃이 유지되게 설정
 
   '@media': {
-    'screen and (max-width: 768px) ': {
+    'screen and (max-width: 1024px) ': {
       width: '100%',
       height: '100%',
       maxWidth: '768px',
