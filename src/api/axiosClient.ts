7c5c62750dd2f433937ed2ac8bc6e0b3f7b872b4
deswaq1220 <deswaq789@naver.com>
2024-10-03 16:00:17 +0900

    chore: defaults.baseURL 적용

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const { VITE_BASE_REQUEST_URL } = import.meta.env;
+axios.defaults.baseURL = VITE_BASE_REQUEST_URL;
 
 export const axiosClient = axios.create({
-  baseURL: VITE_BASE_REQUEST_URL,
   withCredentials:false
 })
