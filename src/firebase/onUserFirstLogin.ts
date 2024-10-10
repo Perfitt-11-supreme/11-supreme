0ffda81d6dbb1a23bab0b2dc058bbc9033b681fe
laurenCho9 <lauren.choux@gmail.com>
2024-10-11 02:25:33 +0900

    chore: 주석 제거

--- src/firebase/onUserFirstLogin.ts
+++ src/firebase/onUserFirstLogin.ts
@@ -1,30 +1,3 @@
-// import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-// import useUidAddStore from '../stores/useUidAddStore';
-// import useUserStore from '../stores/useUserStore';
-
-// export const onUserFirstLogin = async (email: string, password: string) => {
-//   try {
-//     // Firebase 인증 인스턴스 가져오기
-//     const auth = getAuth();
-
-//     // 이메일과 비밀번호로 로그인
-//     const userCredential = await signInWithEmailAndPassword(auth, email, password);
-//     const user = userCredential.user;
-
-//     if (user) {
-//       const { addUidToFirestore } = useUidAddStore();
-//       const { setUser } = useUserStore();
-
-//       // Firestore에 UID 추가
-//       await addUidToFirestore(user.uid);
-
-//       // user.email이 null일 수 있으므로 undefined로 변환
-//       setUser({ uid: user.uid, email: user.email ?? undefined });
-//     }
-//   } catch (error) {
-//     console.error('로그인 실패: ', error);
-//   }
-// };
 import useUidAddStore from '../stores/useUidAddStore';
 import useUserStore from '../stores/useUserStore';
 
